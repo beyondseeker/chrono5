2810ddada927177524dbb794a691fbbb1ff42337
beyondseeker <makoto.sato@objectfanatics.com>
2019-10-04 16:58:19 +0900

    add HelloWorld test

--- app/src/test/java/com/objectfanatics/chrono5/ExampleUnitTest.kt
+++ app/src/test/java/com/objectfanatics/chrono5/ExampleUnitTest.kt
@@ -1,17 +1,22 @@
 package com.objectfanatics.chrono5
 
+import net.bytebuddy.ByteBuddy
+import net.bytebuddy.implementation.FixedValue
+import net.bytebuddy.matcher.ElementMatchers
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+    // @See https://github.com/raphw/byte-buddy#hello-world
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun helloWorld() {
+        val dynamicType = ByteBuddy()
+            .subclass(Any::class.java)
+            .method(ElementMatchers.named("toString"))
+            .intercept(FixedValue.value("Hello World!"))
+            .make()
+            .load(javaClass.classLoader)
+            .loaded
+        assertEquals("Hello World!", dynamicType.newInstance().toString())
     }
 }
