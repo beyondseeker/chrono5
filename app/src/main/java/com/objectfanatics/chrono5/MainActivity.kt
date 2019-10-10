ab1d7751e66c3e1678d1a56bc1047e5503167e3f
beyondseeker <makoto.sato@objectfanatics.com>
2019-10-10 11:04:54 +0900

    Android版HelloWorldを追加。

--- app/src/main/java/com/objectfanatics/chrono5/MainActivity.kt
+++ app/src/main/java/com/objectfanatics/chrono5/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.objectfanatics.chrono5
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import net.bytebuddy.ByteBuddy
+import net.bytebuddy.android.AndroidClassLoadingStrategy
+import net.bytebuddy.implementation.FixedValue
+import net.bytebuddy.matcher.ElementMatchers
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloWorld()
+    }
+
+    fun helloWorld() {
+        val dynamicType = ByteBuddy()
+            .subclass(Any::class.java)
+            .method(ElementMatchers.named("toString"))
+            .intercept(FixedValue.value("Hello World!"))
+            .make()
+            .load(
+                this::class.java.classLoader,
+                AndroidClassLoadingStrategy.Wrapping( application.getDir( "dexgen", Context.MODE_PRIVATE ) )
+            )
+            .loaded
+        Toast.makeText(this, dynamicType.newInstance().toString(), Toast.LENGTH_SHORT).show()
     }
 }
